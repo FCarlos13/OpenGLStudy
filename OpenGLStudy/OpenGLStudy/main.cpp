5fdfeea57286a8f7e50d4bb5fef5bceab23e61da
周琪山 <fcarlos13_cn@outlook.com>
2020-09-20 18:41:33 +0800

    更新笔记，花了个矩形

--- OpenGLStudy/OpenGLStudy/main.cpp
+++ OpenGLStudy/OpenGLStudy/main.cpp
@@ -71,7 +71,8 @@ int main()
 
 	//Generate Shaders
 	GLuint vertexShader, fragmentShader;
-	GLint  success;		GLchar infoLog[512];
+	GLint  success;
+	GLchar infoLog[512];
 
 	//vertex shader
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -112,27 +113,44 @@ int main()
 	glUseProgram(shaderProgram);
 	
 	//detach and delete shaders
-	glDetachShader(vertexShader);
-	glDetachShader(fragmentShader);
+	//glDetachShader(vertexShader);
+	//glDetachShader(fragmentShader);
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
 	//顶点数据
-	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	float vertices[] = 
+	{
+		-0.5f, -0.5f, 0.0f,		//左下
+		 0.5f, -0.5f, 0.0f,		//右下
+		 0.5f,  0.5f, 0.0f,		//右上
+		-0.5f,  0.5f, 0.0f		//左上
 	};
 
-	//VBO
-	unsigned int VBO;
+	unsigned int indices[] = 
+	{			 // 注意索引从0开始! 
+		0, 1, 3, // 第一个三角形
+		1, 2, 3  // 第二个三角形
+	};
+
+	//VBO VAO
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	//应用顶点属性
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	//解绑VAO，防止误操作
+	glBindVertexArray(0);
 
 	//Render loop
 	while (!glfwWindowShouldClose(window))
@@ -144,6 +162,11 @@ int main()
 		//Render construction
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO); 
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(VAO);
 
 		//检查并调用事件，交换缓冲
 		glfwPollEvents();
