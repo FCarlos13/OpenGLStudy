a8cb6bd081559f704c2abf0ed7fac83eda676152
周琪山 <fcarlos13_cn@outlook.com>
2020-09-22 20:29:32 +0800

    3D Programming

--- OpenGLStudy/OpenGLStudy/Resources/Shaders/shader.frag
+++ OpenGLStudy/OpenGLStudy/Resources/Shaders/shader.frag
@@ -1,10 +1,15 @@
 #version 330 core
 
-in vec3 ourColor;
+//in vec3 ourColor;
+in vec2 tP;
 
 out vec4 color;
 
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
 void main()
 {
-	color = vec4(ourColor, 1.0f);
+//	color = texture(texture1, tP);// * vec4(ourColor, 1.0f) // 乘上顶点颜色可以将两者混合
+	color = mix(texture(texture1, tP), texture(texture2, vec2(1.0f - tP.x, tP.y)), 0.2);
 }
--- OpenGLStudy/OpenGLStudy/main.cpp
+++ OpenGLStudy/OpenGLStudy/main.cpp
@@ -10,6 +10,14 @@
 
 #include "Shader.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+//GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 const GLuint WIDTH = 800, HEIGHT = 600;
 
 //function prototype
@@ -55,17 +63,25 @@ int main()
 	//创建视窗
 	glViewport(0, 0, WIDTH, HEIGHT);
 	
+
+
 	//creat shader program
 	Shader ourShader("Resources/Shaders/shader.vs", "Resources/Shaders/shader.frag");
 	
+	
+	/*
+				DATA
+	*/
+
+
 	//顶点数据
 	//SINGLE TRANGLE TEST AND RECTANGLE TEST
 	float vertices[] = 
-	{
-		-0.5f, -0.5f, 0.0f,		//左下
-		 0.5f, -0.5f, 0.0f,		//右下
-		 0.0f,  0.5f, 0.0f,		//右上
-//		-0.5f,  0.5f, 0.0f		//左上
+	{	//position				//texture
+		-0.5f, -0.5f, 0.0f,		0.0f, 0.0f,		//左下
+		 0.5f, -0.5f, 0.0f,		1.0f, 0.0f,		//右下
+		 0.5f,  0.5f, 0.0f,		1.0f, 1.0f,		//右上
+		-0.5f,  0.5f, 0.0f,		0.0f, 1.0f		//左上
 	};
 
 	//TWO TRANGLES TEST
@@ -86,23 +102,31 @@ int main()
 		0, 1, 3, // 第一个三角形
 		1, 2, 3  // 第二个三角形
 	};
+	
 
-	//VBO VAO
-	unsigned int VBO, VAO; //EBO;
+
+	/*
+				VBO VAO
+	*/
+
+
+
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-//	glGenBuffers(1, &EBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	//应用顶点属性
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 	//解绑VBO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	//解绑VAO，防止误操作
@@ -111,7 +135,89 @@ int main()
 	//以线条绘制
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	//Render loop
+
+	/*
+			TEXTURE CODE
+	*/
+
+
+	//Generate texture
+
+	unsigned int texture1, texture2;
+	glGenTextures(1, &texture1);
+	//texture1
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	// 为当前绑定的纹理对象设置环绕、过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// 加载并生成纹理
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char *data = stbi_load("wall.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);			//自动生成所有需要的多级渐远纹理
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	//texture2
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// 加载并生成纹理
+	data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+
+	//load fragment shader's uniform
+	ourShader.use();
+	////glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0); // 手动设置
+	ourShader.setUniInt("texture1", 0);
+	ourShader.setUniInt("texture2", 3); // 或者使用着色器类设置
+	//Shader.setUniInt("xxxx", value);
+	//这里的value是GL_TEXTURE0 + value 所对应的active texture
+
+
+	/*
+				GLM TRANSFORM MATRIX
+	*/
+
+	glm::mat4 model, view, projection; //		vclip = vproj * vview * vmodel *vlocal
+	model		= glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f)); 
+	view		= glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));						// 注意，我们将矩阵向我们要进行移动场景的反方向移动
+	projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+
+	int transUniChange;
+
+
+
+	/*
+				RENDER LOOP
+	*/
+
+
 	while (!glfwWindowShouldClose(window))
 	{
 		//Input:
@@ -121,12 +227,25 @@ int main()
 		//Render construction
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		
+		// bind textures on corresponding texture units
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE3);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+
+		transUniChange = glGetUniformLocation(ourShader.ID, "model");
+		glUniformMatrix4fv(transUniChange, 1, GL_FALSE, glm::value_ptr(model));
+		transUniChange = glGetUniformLocation(ourShader.ID, "view");
+		glUniformMatrix4fv(transUniChange, 1, GL_FALSE, glm::value_ptr(view));
+		transUniChange = glGetUniformLocation(ourShader.ID, "projection");
+		glUniformMatrix4fv(transUniChange, 1, GL_FALSE, glm::value_ptr(projection));
 
 		ourShader.use();
 		//ourShader.setUniFloat("xOffset", 0.5f);
 		glBindVertexArray(VAO); 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-//		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+//		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//glBindVertexArray(0);		//不需要每次都解绑
 
 		//检查并调用事件，交换缓冲
@@ -137,7 +256,9 @@ int main()
 	//de-allocate all resources once they've outlived their purpose:
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-//	glDeleteBuffers(1, &EBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteTextures(1, &texture1);
+	glDeleteTextures(1, &texture2);
 
 	glfwTerminate();
 	return 0;
