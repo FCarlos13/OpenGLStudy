c563da7b4ac068b39d6ef90bbf7c5f8d9438cd67
周琪山 <fcarlos13_cn@outlook.com>
2020-09-24 15:01:22 +0800

    完成fps类3d学习

--- OpenGLStudy/OpenGLStudy/Shader.h
+++ OpenGLStudy/OpenGLStudy/Shader.h
@@ -2,6 +2,9 @@
 #define	SHADER_H
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <string>
 #include <fstream>
@@ -25,6 +28,7 @@ public:
 	void setUniBool(const std::string &name, bool value) const;
 	void setUniInt(const std::string &name, int value) const;
 	void setUniFloat(const std::string &name, float value) const;
+	void setUniMat4(const std::string &name, glm::mat4 &value) const;
 };
 
 
@@ -129,6 +133,11 @@ void Shader::setUniInt(const std::string &name, int value) const
 void Shader::setUniFloat(const std::string &name, float value) const
 {
 	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setUniMat4(const std::string & name, glm::mat4 &value) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
 }
 
 
--- OpenGLStudy/OpenGLStudy/main.cpp
+++ OpenGLStudy/OpenGLStudy/main.cpp
@@ -1,15 +1,9 @@
 
 //glad
 #include <glad/glad.h>
-
 //glfw, "#define..." can avoid the conflict caused by uncorrect sequence call of glad and glfw.
 //#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
-
-#include <iostream>
-
-#include "Shader.h"
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
@@ -18,10 +12,28 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "Shader.h"
+#include "Camera.h"
+
+#include <iostream>
+
+//Settings
 const GLuint WIDTH = 800, HEIGHT = 600;
+
+// camera
+Camera ourCamera(glm::vec3(0.0f, 0.0f, 3.0f));//相机位置在 (0,0,3)的位置，相机类里默认是世界原点
+float lastX = WIDTH / 2.0f;
+float lastY = HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
 
 //function prototype
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 
 int main()
@@ -50,9 +62,21 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	//Initialize glad
+	/*
+			GLFW SETTINGS
+	*/
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	/*
+			GLAD INITIALIZING
+	*/
+
 	//WARNING HERE: glad init must be after making window current.
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -62,27 +86,17 @@ int main()
 
 	//创建视窗
 	glViewport(0, 0, WIDTH, HEIGHT);
-	
+	glEnable(GL_DEPTH_TEST);
 
+	/*
+			CREAT SHADER PROJECT
+	*/
 
-	//creat shader program
 	Shader ourShader("Resources/Shaders/shader.vs", "Resources/Shaders/shader.frag");
-	
 	
 	/*
 				DATA
 	*/
-
-
-	//顶点数据
-	//SINGLE TRANGLE TEST AND RECTANGLE TEST
-	//float vertices[] = 
-	//{	//position				//texture
-	//	-0.5f, -0.5f, 0.0f,		0.0f, 0.0f,		//左下
-	//	 0.5f, -0.5f, 0.0f,		1.0f, 0.0f,		//右下
-	//	 0.5f,  0.5f, 0.0f,		1.0f, 1.0f,		//右上
-	//	-0.5f,  0.5f, 0.0f,		0.0f, 1.0f		//左上
-	//};
 
 	//More 3D
 	float vertices[] = {
@@ -129,43 +143,49 @@ int main()
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
-	//TWO TRANGLES TEST
+	//顶点数据
+	//SINGLE TRANGLE TEST AND RECTANGLE TEST
 	//float vertices[] = 
-	//{
- //       // first triangle
-	//	-0.9f, -0.5f, 0.0f,  // left 
-	//	-0.0f, -0.5f, 0.0f,  // right
-	//	-0.45f, 0.5f, 0.0f,  // top 
-	//	// second triangle
-	//	0.0f, -0.5f, 0.0f,  // left
-	//	0.9f, -0.5f, 0.0f,  // right
-	//	0.45f, 0.5f, 0.0f   // top 
+	//{	//position				//texture
+	//	-0.5f, -0.5f, 0.0f,		0.0f, 0.0f,		//左下
+	//	 0.5f, -0.5f, 0.0f,		1.0f, 0.0f,		//右下
+	//	 0.5f,  0.5f, 0.0f,		1.0f, 1.0f,		//右上
+	//	-0.5f,  0.5f, 0.0f,		0.0f, 1.0f		//左上
 	//};
-
 	//unsigned int indices[] = 
 	//{			 // 注意索引从0开始! 
 	//	0, 1, 3, // 第一个三角形
 	//	1, 2, 3  // 第二个三角形
 	//};
 	
-
+	glm::vec3 cubePositions[] = 
+	{
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
 
 	/*
 				VBO VAO
 	*/
 
-
-
 	unsigned int VBO, VAO;// EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-//	glGenBuffers(1, &EBO);
+	//glGenBuffers(1, &EBO);
 
-	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindVertexArray(VAO);
 
 	//应用顶点属性
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
@@ -180,11 +200,9 @@ int main()
 	//以线条绘制
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-
 	/*
 			TEXTURE CODE
 	*/
-
 
 	//Generate texture
 
@@ -235,16 +253,18 @@ int main()
 	stbi_image_free(data);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-
 	//load fragment shader's uniform
 	ourShader.use();
-	////glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0); // 手动设置
 	ourShader.setUniInt("texture1", 0);
-	ourShader.setUniInt("texture2", 3); // 或者使用着色器类设置
+	ourShader.setUniInt("texture2", 3); 
 	//Shader.setUniInt("xxxx", value);
 	//这里的value是GL_TEXTURE0 + value 所对应的active texture
 
+	/*
+			Matrix variation definition
+	*/
 
+	glm::mat4 model, view, projection; //		vclip = vproj * vview * vmodel * vlocal
 
 	/*
 				RENDER LOOP
@@ -253,13 +273,18 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
+		// per-frame time logic
+		float currentFrame = (float)glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		//Input:
 		//check if press esc
 		processInput(window);
 
 		//Render construction
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		// bind textures on corresponding texture units
 		glActiveTexture(GL_TEXTURE0);
@@ -267,33 +292,49 @@ int main()
 		glActiveTexture(GL_TEXTURE3);
 		glBindTexture(GL_TEXTURE_2D, texture2);	
 
-	/*
-				GLM TRANSFORM MATRIX
-	*/
-
-		glm::mat4 model, view, projection; //		vclip = vproj * vview * vmodel * vlocal
-		unsigned int transUniChange1, transUniChange2, transUniChange3;
-		// specific code are in RENDER LOOP
-		// here only gives out the definition
-
-
-
-		model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));						// 注意，我们将矩阵向我们要进行移动场景的反方向移动
-		projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
-
-		transUniChange1 = glGetUniformLocation(ourShader.ID, "model");
-		glUniformMatrix4fv(transUniChange1, 1, GL_FALSE, glm::value_ptr(model));
-		transUniChange2 = glGetUniformLocation(ourShader.ID, "view");
-		glUniformMatrix4fv(transUniChange2, 1, GL_FALSE, &view[0][0]);
-//		glUniformMatrix4fv(transUniChange2, 1, GL_FALSE, glm::value_ptr(view));
-		transUniChange3 = glGetUniformLocation(ourShader.ID, "projection");
-		glUniformMatrix4fv(transUniChange3, 1, GL_FALSE, glm::value_ptr(projection));
 
 		ourShader.use();
 		//ourShader.setUniFloat("xOffset", 0.5f);
+
+		/*
+				projection matrix
+		*/
+
+		projection = glm::perspective(glm::radians(ourCamera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+		ourShader.setUniMat4("projection", projection);
+		/*
+				view/camera matrix
+		*/
+
+		//move by ur keys
+		view = ourCamera.GetViewMatrix();
+		ourShader.setUniMat4("view", view);
+
+		// camera/view transformation (ROTATION)
+		//float radius = 10.0f;
+		//float camX = (float)sin(glfwGetTime()) * radius;
+		//float camZ = (float)cos(glfwGetTime()) * radius;
+		//view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
 		glBindVertexArray(VAO); 
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	/*
+			model matrix
+	*/
+
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model;
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			if (i % 3 == 0)  // every 3rd iteration (including the first) we set the angle using GLFW's time function.
+				angle = (float)glfwGetTime() * 25.0f;
+			model = glm::rotate(model, /*(float)glfwGetTime()*/glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			ourShader.setUniMat4("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+//		glDrawArrays(GL_TRIANGLES, 0, 36);
 //		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//glBindVertexArray(0);		//不需要每次都解绑
 
@@ -317,8 +358,40 @@ void framebuffer_size_callback(GLFWwindow * window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
+
+void mouse_callback(GLFWwindow * window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+	ourCamera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	ourCamera.ProcessMouseScroll(yoffset);
+}
+
 void processInput(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		ourCamera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		ourCamera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		ourCamera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		ourCamera.ProcessKeyboard(RIGHT, deltaTime);
 }
