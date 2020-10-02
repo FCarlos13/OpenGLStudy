2f75311722769ea48db4a7a06055aab1342dd8fe
周琪山 <fcarlos13_cn@outlook.com>
2020-10-02 10:32:26 +0800

    添加Assimp

---
+++ OpenGLStudy/OpenGLStudy/Model.h
@@ -0,0 +1,85 @@
+#pragma once
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "Shader.h"
+#include "Mesh.h"
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Model
+{
+public:
+	/*  函数   */
+	Model(char *path){ loadModel(path); }
+	void Draw(Shader shader);
+private:
+	/*  模型数据  */
+	vector<Mesh> meshes;
+	string directory;
+	/*  函数   */
+	void loadModel(string path);
+	void processNode(aiNode *node, const aiScene *scene);
+	Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+	vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type,
+		string typeName);
+};
+
+void Model::Draw(Shader shader)
+{
+	for (unsigned int i = 0; i < meshes.size(); i++)
+		meshes[i].Draw(shader);
+}
+
+void Model::loadModel(string path)
+{
+	Assimp::Importer importer;
+	const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+	{
+		cout << "ERROR::ASSIMP::" << importer.GetErrorString() << endl;
+		return;
+	}
+	directory = path.substr(0, path.find_last_of('/'));
+
+	processNode(scene->mRootNode, scene);
+}
+
+void Model::processNode(aiNode *node, const aiScene *scene)
+{
+	// 处理节点所有的网格（如果有的话）
+	for (unsigned int i = 0; i < node->mNumMeshes; i++)
+	{
+		aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+		meshes.push_back(processMesh(mesh, scene));
+	}
+	// 接下来对它的子节点重复这一过程
+	for (unsigned int i = 0; i < node->mNumChildren; i++)
+	{
+		processNode(node->mChildren[i], scene);
+	}
+}
+
+ Mesh Model::processMesh(aiMesh * mesh, const aiScene * scene)
+{
+	return ;
+}
+
+vector<Texture> Model::loadMaterialTextures(aiMaterial * mat, aiTextureType type, string typeName)
+ {
+	 return ;
+ }
+
+
+#endif // !MODEL_H
+
+
+
